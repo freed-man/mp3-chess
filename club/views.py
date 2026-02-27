ad0800b6dc3e948ad15459124d3e6eea847712df
freed-man <rolandk@proton.me>
2026-02-27 19:32:37 +0000

    added topic detail page with comments display

--- club/views.py
+++ club/views.py
@@ -1,5 +1,6 @@
+from django.shortcuts import render, get_object_or_404
 from django.views import generic
-from .models import Topic
+from .models import Topic, Comment
 
 
 class TopicList(generic.ListView):
@@ -9,3 +10,23 @@ class TopicList(generic.ListView):
     queryset = Topic.objects.filter(status=1)
     template_name = "club/index.html"
     paginate_by = 6
+
+
+def topic_detail(request, slug):
+    """
+    Displays a single topic with its comments.
+    """
+    queryset = Topic.objects.filter(status=1)
+    topic = get_object_or_404(queryset, slug=slug)
+    comments = topic.comments.all().order_by("created_on")
+    comment_count = topic.comments.count()
+
+    return render(
+        request,
+        "club/topic_detail.html",
+        {
+            "topic": topic,
+            "comments": comments,
+            "comment_count": comment_count,
+        },
+    )
