eeffef6a70ebdc40a293fd28355b03bcf8f59a81
freed-man <rolandk@proton.me>
2026-02-28 22:30:32 +0000

    added edit and delete comment functionality

--- club/urls.py
+++ club/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('<slug:slug>/', views.topic_detail, name='topic_detail'),
     path('<slug:slug>/upvote/', views.topic_vote, {'vote_type': 1}, name='topic_upvote'),
     path('<slug:slug>/downvote/', views.topic_vote, {'vote_type': -1}, name='topic_downvote'),
+    path('<slug:slug>/edit_comment/<int:comment_id>', views.comment_edit, name='comment_edit'),
+    path('<slug:slug>/delete_comment/<int:comment_id>', views.comment_delete, name='comment_delete'),
 ]
--- club/views.py
+++ club/views.py
@@ -79,3 +79,41 @@ def topic_vote(request, slug, vote_type):
             messages.add_message(request, messages.SUCCESS, 'Vote recorded!')
 
     return HttpResponseRedirect(reverse('topic_detail', args=[slug]))
+
+
+def comment_edit(request, slug, comment_id):
+    """
+    Allows a user to edit their own comment.
+    """
+    queryset = Topic.objects.filter(status=1)
+    topic = get_object_or_404(queryset, slug=slug)
+    comment = get_object_or_404(Comment, pk=comment_id)
+    comment_form = CommentForm(data=request.POST, instance=comment)
+
+    if request.method == "POST":
+        if comment_form.is_valid() and comment.user == request.user:
+            comment = comment_form.save(commit=False)
+            comment.topic = topic
+            comment.save()
+            messages.add_message(request, messages.SUCCESS, 'Comment updated!')
+        else:
+            messages.add_message(request, messages.ERROR, 'Error updating comment!')
+
+    return HttpResponseRedirect(reverse('topic_detail', args=[slug]))
+
+
+def comment_delete(request, slug, comment_id):
+    """
+    Allows a user to delete their own comment.
+    """
+    queryset = Topic.objects.filter(status=1)
+    topic = get_object_or_404(queryset, slug=slug)
+    comment = get_object_or_404(Comment, pk=comment_id)
+
+    if comment.user == request.user:
+        comment.delete()
+        messages.add_message(request, messages.SUCCESS, 'Comment deleted!')
+    else:
+        messages.add_message(request, messages.ERROR, 'You can only delete your own comments!')
+
+    return HttpResponseRedirect(reverse('topic_detail', args=[slug]))
